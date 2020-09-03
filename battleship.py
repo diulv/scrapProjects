070cb24476dbb7b9e65b9203c0491ae308ee3f34
diulv <danielclaywillmott@gmail.com>
2020-09-03 15:08:50 -0400

    create random grid

--- battleship.py
+++ battleship.py
@@ -1,20 +1,22 @@
 import os
+import random
 
 userPrimaryGrid = [['.' for x in range(11)] for y in range(11)]
 userTargetGrid = [['.' for x in range(11)] for y in range(11)]
-computerMap = [['.' for x in range(11)] for y in range(11)]
+computerPrimaryGrid = [['.' for x in range(11)] for y in range(11)]
+computerTargetGrid = [['.' for x in range(11)] for y in range(11)]
 SHIPS = [["Carrier", 5], ["Battleship", 4], ["Cruiser", 3], ["Submarine", 3], ["Destroyer", 2]]
 
-def createMap(map):
+def createMap(grid):
     #blank top left
-    map[0][0] = "  "
+    grid[0][0] = "  "
     #create alpha column header
     letter = 64
     for col in range(1, 11):
-        map[0][col] = chr(letter + col)
+        grid[0][col] = chr(letter + col)
     #create numeric row header
     for row in range(1, 11):
-        map[row][0] = "{:02d}".format(row)
+        grid[row][0] = "{:02d}".format(row)
     
 
 def printScreen(primaryMap, targetMap):
@@ -30,7 +32,7 @@ def printScreen(primaryMap, targetMap):
             print(str(value) + " ", end='')
         print('')
         
-def placeShips(map):
+def placeShips(grid):
     for ship in SHIPS:
         while True:
             name, length = ship[0], ship[1]
@@ -53,49 +55,74 @@ def placeShips(map):
                 if dir == "right" or dir == "down":
                     break
                 print('Invalid direction')
-            if shipOnFreeSpace(col, row, length, map, dir):
+            if shipOnFreeSpace(col, row, length, grid, dir):
                 break
             else:
                 print("Ship is not on a valid space, please try again")
-        placeShip(col, row, length, map, dir)
-        printScreen(map, userTargetGrid)
+        placeShip(col, row, length, grid, dir)
+        printScreen(grid, userTargetGrid)
                     
 #if each space we place a ship is not free or valid, return false
-def shipOnFreeSpace(col, row, len, map, dir):
+def shipOnFreeSpaces(col, row, len, grid, dir):
     print("in")
     if dir == "down":
         print("2")
         try:
             for i in range(len):
-                print(' row= ' + str(row + i) + 'col= ' + str(col) + ' space= ' + str(map[row+i][col]))
-                if map[row + i][col] != '.':
+                print(' row= ' + str(row + i) + 'col= ' + str(col) + ' space= ' + str(grid[row+i][col]))
+                if grid[row + i][col] != '.':
                     return False
         except:
             return False
     elif dir == "right":
         try:
             for i in range(len):
-                print(' row= ' + str(row) + 'col= ' + str(col+i) + ' space= ' + str(map[row][col+i]))
-                if map[row][col + i] != '.':
+                print(' row= ' + str(row) + 'col= ' + str(col+i) + ' space= ' + str(grid[row][col+i]))
+                if grid[row][col + i] != '.':
                     return False
         except:
             return False
     return True
 
-def placeShip(col, row, len, map, dir):
+def placeShip(col, row, len, grid, dir):
     if dir == "down":
         for i in range(len):
-            map[row + i][col] = u"\u25A0"
+            grid[row + i][col] = u"\u25A0"
     elif dir == "right":
         for i in range(len):
-            map[row][col + i] = u"\u25A0"
+            grid[row][col + i] = u"\u25A0"
 
-def createRandomBoard():
-    return 0
+def createRandomGrid(grid):
+    for ship in SHIPS:
+        length = ship[1]
+        while True:
+            col = random.randrange(1,11)
+            row = random.randrange(1,11)
+            if random.randrange(2) == 0:
+                dir = "down"
+            else:
+                dir = "right"
+                print("col= " + str(col) + " row= " + str(row) + "dir= " + str(dir))
+            if shipOnFreeSpaces(col, row, length, grid, dir):
+                break
+        placeShip(col, row, length, grid, dir)
         
+        
+#game
+    #turn
+        #player guesses space
+        #if correct, write X on computerPrimaryGrid and playerTargetGrid
+        #if wrong, write O on computerPrimaryGrid and playerTargetGrid
+        #if 
+        
+
 os.system('cls')
 createMap(userPrimaryGrid)
 createMap(userTargetGrid)
+createMap(computerPrimaryGrid)
+createRandomGrid(computerPrimaryGrid)
+printScreen(computerPrimaryGrid, computerTargetGrid)
+input(" stop")
 printScreen(userPrimaryGrid, userTargetGrid)
 placeShips(userPrimaryGrid)
 #print(userMap)
