7c91079637f63b3cd43637b5e165ee9749a2a079
diulv <danielclaywillmott@gmail.com>
2020-09-06 20:46:36 -0400

    created folders, skipBo pseudocode

---
+++ skipBo/skipBo.py
@@ -0,0 +1,16 @@
+144 numbered cards + 18 wild cards
+
+each player has 4 discard piles, a stock pile
+4 central discard piles, central draw pile
+
+game setup
+    30 cards dealt to each player face down, forms the stock pile
+    flip top card of stock pile over
+    
+player's turn
+    draw back up to 5
+    
+    if no cards in hand, draw back to 5
+    either start a new pile or continue a pile in cronological order from hand or top of stock pile
+    
+    
